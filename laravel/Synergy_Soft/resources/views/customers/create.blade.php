49810fac79c8b272ad1d79a9c58691afc652fdcc
Unknown <rm150928@edu.rocwb.nl>
2017-10-10 13:04:55 +0200

    Auto stash before merge of "master" and "origin/master"

--- laravel/Synergy_Soft/resources/views/customers/create.blade.php
+++ laravel/Synergy_Soft/resources/views/customers/create.blade.php
@@ -5,5 +5,44 @@
 @endsection
 
 @section('mainbar')
-<p>hier staat nu zooi</p>
+
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="Name">Compeny Name</label>
+            <input class="form-control" type="text" name="Name" placeholder="Name">
+        </div>
+
+        <div class="form-group">
+            <label for="Residence">residence 1</label>
+            <input class="form-control" type="text" name="Residence" placeholder="Residence">
+        </div>
+        <div class="form-group">
+            <label for="Adress">Adress 1</label>
+            <input class="form-control" type="text" name="Adress" placeholder="Adress">
+        </div>
+        <div class="form-group">
+            <label for="houseNumber">house number</label>
+            <input type="text" name="houseNumber" placeholder="house number:" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="zipCode">Zip codes</label>
+            <input type="text" name="ZipCode" placeholder="Zip code" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="phoneNumber">phone number</label>
+            <input type="text" name="phoneNumber" placeholder="0653338519" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="email" name="email" placeholder="hoi@barocit.nl" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="contact">Contact person</label>
+            <input type="text" name="contact" placeholder="Jaapy Kreekel" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary">
+        </div>
+    </form>
+
 @endsection
