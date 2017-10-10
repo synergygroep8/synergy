71de4e96a5ce67f3cfbfe50d12bd27161cd3d136
Jtech123 <alt.sa-8rme46w@yopmail.com>
2017-10-10 09:03:11 +0200

    Database Update

--- laravel/Synergy_Soft/database/migrations/2017_10_02_071703_create_users_table.php
+++ laravel/Synergy_Soft/database/migrations/2017_10_02_071703_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->integer('department');
             $table->timestamps();
+            $table->date('deleted_at')->nullable();
         });
     }
 
--- laravel/Synergy_Soft/database/seeds/DatabaseSeeder.php
+++ laravel/Synergy_Soft/database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         {
             $this->call(TblCustomersSeeder::class);
         }
+        $this->call(UserSeeder::class);
     }
 }
