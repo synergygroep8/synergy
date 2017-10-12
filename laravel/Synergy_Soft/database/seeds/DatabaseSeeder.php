3ed8ee93dbba54496c103e158d61de81aff40575
Jtech123 <alt.sa-8rme46w@yopmail.com>
2017-10-12 15:17:08 +0200

    Database seeder update

--- laravel/Synergy_Soft/database/seeds/DatabaseSeeder.php
+++ laravel/Synergy_Soft/database/seeds/DatabaseSeeder.php
@@ -12,10 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        for ($i = 0; $i < 100; $i++)
+        for ($i = 0; $i < 200; $i++)
         {
-            $this->call(TblCustomersSeeder::class);
+            //$this->call(TblCustomersSeeder::class);
+            $this->call(ProjectSeeder::class);
         }
-        $this->call(UserSeeder::class);
+        //$this->call(UserSeeder::class);
+
     }
 }
